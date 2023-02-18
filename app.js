67cbb739bd3f89228e6adde124956a839235cd7a
= <iarlensr@hotmail.com>
2023-02-18 19:07:45 -0300

    Resolvendo problema de cors.

--- app.js
+++ app.js
@@ -30,7 +30,15 @@ const corsOptions = {
   },
 };
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
 app.use(cors(corsOptions));
 
 // Upload directory
